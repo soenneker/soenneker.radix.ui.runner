fe91baae8d6560b68f9dffdc0097b5d8dbf045ef
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:22 -0500

    xml commenting

--- src/Soenneker.Radix.Ui.Runner/Constants.cs
+++ src/Soenneker.Radix.Ui.Runner/Constants.cs
@@ -1,10 +1,28 @@
 namespace Soenneker.Radix.Ui.Runner;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The radix ui repository.
+    /// </summary>
     public const string RadixUiRepository = "https://github.com/radix-ui/website.git";
+    /// <summary>
+    /// The crawled repository.
+    /// </summary>
     public const string CrawledRepository = "https://github.com/soenneker/soenneker.radix.ui.crawled.git";
+    /// <summary>
+    /// The components repository.
+    /// </summary>
     public const string ComponentsRepository = "https://github.com/soenneker/soenneker.radix.ui.components.git";
+    /// <summary>
+    /// The components url.
+    /// </summary>
     public const string ComponentsUrl = "https://www.radix-ui.com/primitives/docs/components";
+    /// <summary>
+    /// The commit message.
+    /// </summary>
     public const string CommitMessage = "Refresh Radix component docs HTML";
 }
