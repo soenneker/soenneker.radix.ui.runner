3c49ea6c7c7a576e84f5448bab8568520b82124f
soenneker <jake@soenneker.com>
2026-08-29 14:51:04 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Radix.Ui.Runner/Utils/FileOperationsUtil.cs
+++ src/Soenneker.Radix.Ui.Runner/Utils/FileOperationsUtil.cs
@@ -21,7 +21,7 @@ using Soenneker.Utils.Path.Abstract;
 
 namespace Soenneker.Radix.Ui.Runner.Utils;
 
-///<inheritdoc cref="IFileOperationsUtil"/>
+/// <inheritdoc cref="IFileOperationsUtil"/>
 public sealed class FileOperationsUtil : IFileOperationsUtil
 {
     private readonly ILogger<FileOperationsUtil> _logger;
